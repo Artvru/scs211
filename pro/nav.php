61780093544f389ede1ce6176be9821776c367db
Artvru <151433841+Artvru@users.noreply.github.com>
2024-03-01 22:14:34 +0700

    nav.php

---
+++ pro/nav.php
@@ -0,0 +1,62 @@
+<div class="container bg">
+    <div class="navBar">
+      <div class="logo">
+        <a href="#">
+          <i class="fas fa-suitcase-rolling"></i>
+        </a>
+      </div>
+      <ul>
+        <li><a href="Pro1.php">Home</a></li>
+        <li><a href="Pro2.php">Work Experience</a></li>
+        <li><a href="Pro3.php">About</a></li>
+        <li><a href="Pro4.php">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="content">
+      <div class="phuket" style="width:50%;">
+        <div class="tnx">
+          <h2><i class="fas fa-map-marker-alt"></i> Bangkok </h2>
+        </div>
+        <div class="txt">
+          <h1>EN</h1>
+          <h1>TECH</h1>
+        </div>
+        <p>The company provides services and consulting services in the field of design of high voltage and <br>
+          electrical communication systems, as well as engineering calculations. Installation of transformers and
+          related equipment nationwide</p>
+      </div>
+      <div class="card" style="width:50%;">
+        <a href="https://www.google.com/" target="_blank">
+          <div class="box1">
+            <h4 style="color: white;">Overhead Line Construction</h4>
+          </div>
+        </a>
+        <a href="https://www.google.com/" target="_blank">
+          <div class="box2">
+            <h4 style="color: white;">Underground Cable Construction</4>
+          </div>
+        </a>
+        <a href="https://www.google.com/" target="_blank">
+          <div class="box3">
+            <h4 style="color: white;"> Electrical System Construction </h4>
+          </div>
+        </a>
+        <a href="https://www.google.com/" target="_blank">
+          <div class="box4">
+            <h4 style="color: white;"> Fire Protection System </h4>
+          </div>
+        </a>
+        <!--       <div class="box2">
+        <h2>Island</h2>
+      </div>
+      <div class="box3">
+        <h2>Mountain</h2>
+      </div>
+      <div class="box4">
+        <h2>Scenery</h2>
+      </div> -->
+      </div>
+    </div>
+
+  </div> <!-- End Container Bg-->
